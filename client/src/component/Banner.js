eb758b84ba4d9c50cdd885ebcd4321610f4b35e2
clooney92 <kurashikijun@gmail.com>
2022-09-20 21:16:48 +0900

    Feat : 인트로 페이지 및 프로젝트 초기 세팅

---
+++ client/src/component/Banner.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: #1B6DFF;
+    width: 315px;
+    height: 410px;
+    border-radius: 30px;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+    color: #FFDE32;
+    line-height: 1.75;
+`
+
+const Banner = () => {
+    return (
+        <>
+            <Container>
+                <h2 style={{paddingBottom: "24px", fontSize: "25px"}}>질문&답변 게시판입니다.</h2>
+                <h3 style={{color: "white", fontSize: "18px"}}>
+                    이곳은 한국어 공부와 관련 된<br/>
+                    질문과답변을 하는 곳 입니다.<br/>
+                    모르는 문법이나 어휘를<br/>
+                    자유롭게 질문하고 답변해주세요.<br/>
+                    한국어가 서투른 친구들에게<br/>
+                    큰 힘이 됩니다.                
+                </h3>
+            </Container>
+        </>
+    )
+}
+
+export default Banner;
---
+++ client/src/component/NavigationBar.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import { ReactComponent as Navlogo } from "../assets/fa0ea0cdc34fd733.svg"
+import Button from "./Button";
+
+const Container = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #1B6DFF;
+    margin: 0;
+    padding: 0;
+`
+
+const Navbar = () => {
+    return (
+        <>
+            <Container>
+                <Navlogo style={{height:80}}/>
+                <div>
+                    <Button fontSize="1.1rem">질문&답변</Button>
+                    <Button fontSize="1.1rem">정보 공유</Button>
+                    <Button fontSize="1.1rem">친구 찾기</Button>
+                </div>
+                <div>
+                    <Button fontSize="0.9rem">회원가입</Button>
+                    <Button fontSize="0.9rem">로그인</Button>
+                </div>
+            </Container>
+        </>
+    )
+}
+
+export default Navbar;
